c9ab80723761316140c65fb8f9314a485b339749
Clewerton Roque <clewerton203@gmail.com>
2020-04-19 15:10:43 -0300

    Calculator finished

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as Font from 'expo-font';
+import {useState} from 'react'
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 import { Platform, StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
@@ -13,33 +13,98 @@ export default function App() {
     'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
   });
 
-  return (
+  const [ currentNumber, setCurrentNumber ] = useState("")
+  const [ lastNumber, setLastNumber ] = useState("")
+
+
+  function calculator(){
+    const splitNumbers = currentNumber.split(" ")
+    const operator = splitNumbers[1]
+
+    if(operator === "*"){    
+      setCurrentNumber(parseInt(splitNumbers[0]) * parseInt(splitNumbers[2]))
+      return
+    }
+    if(operator === "/"){
+      setCurrentNumber(parseInt(splitNumbers[0]) / parseInt(splitNumbers[2]))
+      return
+    }
+    if(operator === "+"){
+
+      setCurrentNumber(parseInt(splitNumbers[0]) + parseInt(splitNumbers[2]))
+      return
+    }
+    if(operator === "-"){
+      setCurrentNumber(parseInt(splitNumbers[0]) - parseInt(splitNumbers[2]))
+      return
+    }
+    
+    
+
+  }
+  
+  function handleInput(keyboardPressed){
+
+    if(keyboardPressed === "="){
+      console.log("Number")
+      console.log(currentNumber)
+      setLastNumber(currentNumber + " = ")
+      calculator()
+      return
+    }
+    if(keyboardPressed === "*" | keyboardPressed === "/" | keyboardPressed === "+" | keyboardPressed === "-"){    
+      setCurrentNumber(currentNumber + " " + keyboardPressed + " ")      
+      return
+    }
+    if(keyboardPressed === "DEL"){
+      return
+    }
+    if(keyboardPressed === "."){
+      return
+    }
+    if(keyboardPressed === "+/-"){
+      return
+    }
+    if(keyboardPressed === "AC"){
+      setLastNumber("")
+      setCurrentNumber("")
+      return
+    }
+
+    setCurrentNumber(currentNumber + keyboardPressed)
+
+  }
+
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }return (
     <View style={styles.container}>
       <View style={styles.results}>
-        <Text style={styles.historyText}>200 + 200 =</Text>
-        <Text style={styles.resultText}>400</Text>
+        <Text style={styles.historyText}>{lastNumber}</Text>
+        <Text style={styles.resultText}>{currentNumber}</Text>
       </View>
       <View style={styles.buttons}>
-        <TouchableOpacity style={styles.button}><Text style={[styles.textButton, {color: "#ffbf60"}]}>AC</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>Del</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>%</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>/</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>7</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>8</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>9</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>*</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>4</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>5</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>6</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>-</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>1</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>2</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>3</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>+</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>0</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>.</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.button}><Text style={styles.textButton}>+/-</Text></TouchableOpacity>
-        <TouchableOpacity style={[styles.button, { backgroundColor: '#ffbf60',borderRadius: 50,}]}><Text style={styles.textButton}>=</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("AC")}><Text style={[styles.textButton, {color: "#ffbf60"}]}>AC</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("DEL")}><Text style={styles.textButton}>Del</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("%")}><Text style={styles.textButton}>%</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("/")}><Text style={styles.textButton}>/</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("7")}><Text style={styles.textButton}>7</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("8")}><Text style={styles.textButton}>8</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("9")}><Text style={styles.textButton}>9</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("*")}><Text style={styles.textButton}>*</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("4")}><Text style={styles.textButton}>4</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("5")}><Text style={styles.textButton}>5</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("6")}><Text style={styles.textButton}>6</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("-")}><Text style={styles.textButton}>-</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("1")}><Text style={styles.textButton}>1</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("2")}><Text style={styles.textButton}>2</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("3")}><Text style={styles.textButton}>3</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("+")}><Text style={styles.textButton}>+</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("0")}><Text style={styles.textButton}>0</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput(".")}><Text style={styles.textButton}>.</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleInput("+/-")}><Text style={styles.textButton}>+/-</Text></TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: '#ffbf60',borderRadius: 50,}]} onPress={() => handleInput("=")}><Text style={styles.textButton}>=</Text></TouchableOpacity>
        
       </View>
 
@@ -59,7 +124,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#e5e5e5',
   },
   resultText:{
-    fontFamily: "Roboto-Light",
+    fontFamily: "Roboto-Thin",
     fontWeight: '100',
     color: "#5b5b5b",
     fontSize: 80,
